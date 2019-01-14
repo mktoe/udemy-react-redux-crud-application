27dc9c9630de3aa1b7447c59152354ec24a198ec
mktoe <mktoe1986@gmail.com>
2019-01-14 19:56:04 +0900

    component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
-import React, { Component } from 'react';
+//import React, { Component } from 'react';
 
+/*
 class App extends Component {
   render() {
     const greeting = "HiTom!";
     const dom = <h1>{greeting}</h1>;
     return dom;
   }
+}
+*/
+
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <Cat />
+      <Cat />
+      <Cat />
+      <Cat />
+      <Cat />
+      <Cat />
+    </div>
+
+    )
+}
+
+const Cat = () => {
+  return <div>nya!</div>
 }
 
 export default App;
