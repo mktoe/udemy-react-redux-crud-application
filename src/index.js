c777c4903bf0aedf7792a3b8061df191cd35a220
mktoe <mktoe1986@gmail.com>
2019-01-15 00:33:19 +0900

    store

--- src/index.js
+++ src/index.js
@@ -1,10 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+//createstore インポート
+import { createStore } from 'redux'
+
+//provider インポート
+import { Provider } from 'react-redux'
+
+//作ったreducersをインポート
+import reducer from './reducers'
+
+//App.js を　/component/App.jsに移動してインポート
+import App from './components/App'
+
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//storeの作成
+const store = createStore(reducer)
+
+ReactDOM.render(
+	<Provider store={store}>
+		<App />
+	</Provider>,
+	document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
